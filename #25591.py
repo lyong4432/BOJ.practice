36f8a223aada8beeb175d293b5445be98f012b10
lyong4432 <80114703+lyong4432@users.noreply.github.com>
2023-03-31 13:26:59 +0900

    Create #25591.py

---
+++ #25591.py
@@ -0,0 +1,15 @@
+
+n, m = map(int, input().split())
+a, b = 100-n, 100-m
+
+c = (100) - (a+b)
+d = a*b
+q = d//100
+r = d%100
+
+if d>=100:
+    print(a,b,c,d,q,r)
+    print(c+q,r)
+else:
+    print(a,b,c,d,q,r)
+    print(c,d)
