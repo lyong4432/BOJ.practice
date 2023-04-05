323590885430a277d28b55ad54c9b803b1e1c650
lyong4432 <80114703+lyong4432@users.noreply.github.com>
2023-04-05 09:35:34 +0900

    Create jungol9534.py

---
+++ jungol9534.py
@@ -0,0 +1,8 @@
+arr = ['Carrot', 'Onion', 'Cabbage', 'Potato', 'Sweet Potato', 'Bean'] 
+print('첫 번째 원소:',arr[0])
+print('마지막 원소:',arr[-1])
+brr = list(reversed(arr))
+print('리스트 거꾸로 출력하기:',brr)
+print('li[2:5]:',arr[2:5])
+print('li[5:2:-1]:',arr[5:2:-1])
+print('li[1:6:2]:',arr[1:6:2])
