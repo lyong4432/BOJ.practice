98ed9b4f2a4989765c94140905752b39ff164eb5
Hwajung Choi <80114703+lyong4432@users.noreply.github.com>
2023-05-11 10:31:46 +0900

    Create #23812.py

---
+++ #23812.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+for i in range(n):
+    print('@'*n,' '*n*3,'@'*n,sep='')
+for i in range(n):
+    print('@'*n*5)
+for i in range(n):
+    print('@'*n,' '*n*3,'@'*n,sep='')
+for i in range(n):
+    print('@'*n*5)
+for i in range(n):
+    print('@'*n,' '*n*3,'@'*n,sep='')
