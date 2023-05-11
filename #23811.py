2a7b2269c6e142c81f8afb44ed3d87c3ca78b26d
Hwajung Choi <80114703+lyong4432@users.noreply.github.com>
2023-05-11 10:33:47 +0900

    Create #23811.py

---
+++ #23811.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+
+for i in range(n):
+    print('@'*n*5)
+for i in range(n):
+    print('@'*n)
+for i in range(n):
+    print('@'*n*5)
+for i in range(n):
+    print('@'*n)
+for i in range(n):
+    print('@'*n*5)
