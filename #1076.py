a818ce9940d576531ac3d20ad5fdf2034ca97358
Hwajung Choi <80114703+lyong4432@users.noreply.github.com>
2023-06-09 09:44:40 +0900

    Create #1076.py

---
+++ #1076.py
@@ -0,0 +1,8 @@
+colors = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
+
+a = input()
+b = input()
+c = input()
+
+hap = int(str(colors.index(a))+ str(colors.index(b)))*10**(colors.index(c))
+print(hap)
