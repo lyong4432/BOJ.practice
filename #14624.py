99567d3b9172b9118783bfa6cbe5789026e42237
Hwajung Choi <80114703+lyong4432@users.noreply.github.com>
2023-06-27 09:35:11 +0900

    Create #14624.py

---
+++ #14624.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+if n%2 == 0:
+    print('I LOVE CBNU')
+else:
+    print('*'*n)
+    print(' '*(n//2),'*',sep='')
+    for i in range(1,n//2+1):
+        print(' '*(n//2-i),'*',' '*(2*i-1),'*',sep ='')
