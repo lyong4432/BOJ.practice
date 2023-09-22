aa2faadd32a84e7aa6f76f40205b14f126926075
Hwajung Choi <80114703+lyong4432@users.noreply.github.com>
2023-09-22 11:32:54 +0900

    Create #6810.py

---
+++ #6810.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input().strip())
+n1 = int(input().strip())
+n2 = int(input().strip())
+ 
+hap = 91 + n + n1 * 3 + n2
+print('The 1-3-sum is',hap)
+
+    
+        
+
+
