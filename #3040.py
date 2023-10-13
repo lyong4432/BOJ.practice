b9a97e2ff58165cd96267711b82e66a032794d47
Hwajung Choi <80114703+lyong4432@users.noreply.github.com>
2023-10-13 13:17:41 +0900

    Create #3040.py

---
+++ #3040.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+cm = []
+for i in range(9):
+    cm.append(int(input()))
+
+diff = sum(cm) - 100
+k = 0
+for i in cm:
+    for j in cm: 
+        if i + j == diff and i!=j: 
+            cm.pop(cm.index(i))
+            cm.pop(cm.index(j))
+            k = 1
+            break
+    if k == 1: 
+        break
+
+
+for i in cm: 
+    print(i)
