1d9d8c5aaadb115120f090ad627d9f7f4d73d9e6
Hwajung Choi <80114703+lyong4432@users.noreply.github.com>
2023-11-02 08:57:20 +0900

    Create #1927.py

---
+++ #1927.py
@@ -0,0 +1,18 @@
+import sys
+import heapq as hq
+
+input = sys.stdin.readline
+n = int(input().strip())
+N=[]
+for i in range(n):
+    a= int(input().strip())
+
+    if a == 0: 
+        if not N:
+            print(0)
+        else:
+            print(hq.heappop(N))
+    else: hq.heappush(N,a)
+
+
+
