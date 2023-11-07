4eaf659802148d550dc52538914732ace1f4bcdc
Hwajung Choi <80114703+lyong4432@users.noreply.github.com>
2023-11-07 09:00:41 +0900

    Update #7523.py

--- #7523.py
+++ #7523.py
@@ -5,6 +5,6 @@ input = sys.stdin.readline
 n = int(input().strip())
 for i in range(n):
     a,b = map(int, input().split())
-    r = (b-a+1)*(a+b)/2
+    r = (b-a+1)*(a+b)//2
     print(f'Scenario #{i+1}:\n{r}')
     if i != n-1: print()
