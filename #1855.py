9cec3c8613fc1af2bdb22d2dfef7b23a31f1ed67
Hwajung Choi <80114703+lyong4432@users.noreply.github.com>
2023-11-24 23:27:31 +0900

    Create #1855.py

---
+++ #1855.py
@@ -0,0 +1,22 @@
+import sys
+
+input = sys.stdin.readline
+
+
+
+n = int(input())
+s = input().strip()
+arr = []
+for i in range(0, len(s), n):
+    arr.append(list(s[i:i+n]))
+
+for i in range(len(arr)):
+    if i % 2 != 0:
+        data = list(reversed(arr[i]))
+        arr[i] = data
+
+res = ''
+for j in range(n):
+    for i in range(len(arr)):
+        res += arr[i][j]
+print(res)
