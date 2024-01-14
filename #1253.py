2d005d9a22d57195fa0149fd3b8d01a6b0a4b3fd
Hwajung Choi <80114703+lyong4432@users.noreply.github.com>
2024-01-14 23:39:24 +0900

    Create #1253.py

---
+++ #1253.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+result = 0
+A = list(map(int, input().split()))
+A.sort()
+
+for k in range(n):
+    find = A[k]
+    i = 0
+    j = n-1
+    while i < j:
+        if A[i] + A[j] == find:
+            if i != k and j != k:
+                result +=1
+                break
+            elif i == k:
+                i += 1
+            elif j == k: 
+                j -= 1
+        elif A[i] + A[j] < find: 
+            i += 1
+        else: j -= 1
+    
+print(result)
