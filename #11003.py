ef73eb8dd85f9efad6bc8eb6b584a8bba7ea4c81
Hwajung Choi <80114703+lyong4432@users.noreply.github.com>
2024-01-16 15:42:48 +0900

    Create #11003.py

---
+++ #11003.py
@@ -0,0 +1,15 @@
+import sys 
+from collections import deque
+input = sys.stdin.readline  
+N, L = map(int, input().split())
+mydeque = deque()
+
+now = list(map(int, input().split()))
+
+for i in range(N):
+    while mydeque and mydeque[-1][0] > now[i]:
+        mydeque.pop()
+    mydeque.append((now[i],i))
+    if mydeque[0][1] <= i - L:
+        mydeque.popleft()
+    print(mydeque[0][0], end = ' ')
